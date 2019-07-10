497588b95ecb64b71cf3c574983579f2219240ad
Nikos Karastathis <nkarast@gmail.com>
2019-07-10 16:39:49 +0200

    Updating to v0.2

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='beamCal',
+      version='0.2',
+      description='TODO',
+      url='https://github.com/nkarast/beamCal',
+      author='Nikos Karastathis',
+      author_email='nkarast@gmail.com',
+      license='GPLv3',
+      packages=['beamCal'],
+      zip_safe=False)
